06d513bbdf86d34fbbfc31d9722cd373b9283e5c
Miguel Rodriguez <miguel.rodriguez18@unmsm.edu.pe>
2021-07-02 01:51:41 -0500

    updated ui

--- routes/solicitud.js
+++ routes/solicitud.js
@@ -1,35 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/contacto',(req,res)=>{
-    res.render('formCrearMensaje', {errors: []});
-})
+router.get("/contacto", (req, res) => {
+  res.render("formCrearMensaje", { errors: [] });
+});
 
-router.post('/contacto/', (req,res) => {
-    console.log('SUBMIT');
-    const {firstName, lastName, 
-        dni, telephone, email, reason, comments } = req.body;
+router.post("/contacto/", (req, res) => {
+  console.log("SUBMIT");
+  const { firstName, lastName, dni, telephone, email, reason, comments } =
+    req.body;
 
-    const errors = [];
+  const errors = [];
 
-    if(firstName === undefined || firstName===""){
-        errors.push({text: 'Por favor, escriba su nombres'});
-    }
+  if (firstName === undefined || firstName === "") {
+    errors.push({ text: "Por favor, escriba su nombres" });
+  }
 
-    if(lastName === undefined ||lastName ===""){
-        errors.push({text: 'Por favor, escriba su nombres'});
-    }
+  if (lastName === undefined || lastName === "") {
+    errors.push({ text: "Por favor, escriba su nombres" });
+  }
 
-    console.log(errors);
-    if(errors.length > 0){
-        res.render('formCrearMensaje',{
-            errors,
-            'prevInfo': req.body
-        });
-    }
-    else{
-        res.send('Ok');
-    }
-})
+  console.log(errors);
+  if (errors.length > 0) {
+    res.render("formCrearMensaje", {
+      errors,
+      prevInfo: req.body,
+    });
+  } else {
+    res.send("Solicitud Enviada");
+  }
+});
 
 module.exports = router;
